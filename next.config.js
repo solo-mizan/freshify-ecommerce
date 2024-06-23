d8cee757a13908a5e3f421645f2a34c6466c0c2e
solo-mizan <mizanbd.eee@gmail.com>
2024-06-23 12:52:05 +0600

    fix: spelling correction

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const redirects = require('./redirects')
 
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: truncate,
+    ignoreBuildErrors: true,
   },
   reactStrictMode: true,
   swcMinify: true,
